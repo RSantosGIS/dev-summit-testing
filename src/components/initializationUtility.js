8c1738dff79eb377ea98044b2107cf9292165ebf
Rob Santos <rob9575@esri.com>
2022-02-18 12:25:42 -0600

    refactored utility to expose all initialization methods for unit testing

--- src/components/initializationUtility.js
+++ src/components/initializationUtility.js
@@ -22,79 +22,83 @@ const initializationUtility = {
 
   initWidgets : (sceneView, topBarRef, distanceBtnRef, areaBtnRef) => {
     // add the toolbar for the measurement widgets
-    var activeWidget;
     sceneView.ui.add(topBarRef, 'top-right');
   
     //event handler for when the distance button is clicked - toggles activity
     distanceBtnRef.addEventListener("click", (event) => {
-      setActiveWidget(null);
+      initializationUtility.setActiveWidget(null, sceneView, distanceBtnRef, areaBtnRef);
       if (!event.target.classList.contains("active")) {
-        setActiveWidget("distance");
+        initializationUtility.setActiveWidget("distance", sceneView, distanceBtnRef, areaBtnRef);
       } else {
-        setActiveButton(null);
+        initializationUtility.setActiveButton(null, sceneView);
       }
     });
   
     //event handler for when the area button is clicked - toggles activity
     areaBtnRef.addEventListener("click", (event) => {
-      setActiveWidget(null);
+      initializationUtility.setActiveWidget(null);
       if (!event.target.classList.contains("active")) {
-        setActiveWidget("area");
+        initializationUtility.setActiveWidget("area", sceneView, distanceBtnRef, areaBtnRef);
       } else {
-        setActiveButton(null);
+        initializationUtility.setActiveButton(null,sceneView);
       }
     });
   
-    /**
+    
+  
+    
+  },
+
+  /**
      * toggles the active widget based on the supplied widget type
      * @param {*} type 
      */
-    function setActiveWidget(type) {
-      switch (type) {
-        case "distance":
-          activeWidget = new DirectLineMeasurement3D({
-            view: sceneView
-          });
-  
-          // skip the initial 'new measurement' button
-          activeWidget.viewModel.start();
-  
-          sceneView.ui.add(activeWidget, 'top-right');
-          setActiveButton(distanceBtnRef);
-          break;
-        case "area":
-          activeWidget = new AreaMeasurement3D({
-            view: sceneView
-          });
-  
-          // skip the initial 'new measurement' button
-          activeWidget.viewModel.start();
-  
-          sceneView.ui.add(activeWidget, 'top-right');
-          setActiveButton(areaBtnRef);
-          break;
-        case null:
-        default:
-          if (activeWidget) {
-            sceneView.ui.remove(activeWidget);
-            activeWidget.destroy();
-            activeWidget = null;
-          }
-          break;
-      }
+   setActiveWidget: (type, sceneView, distanceBtnRef, areaBtnRef)=> {
+    var activeWidget = null;
+    switch (type) {
+      case "distance":
+        activeWidget = new DirectLineMeasurement3D({
+          view: sceneView
+        });
+
+        // skip the initial 'new measurement' button
+        activeWidget.viewModel.start();
+
+        sceneView.ui.add(activeWidget, 'top-right');
+        initializationUtility.setActiveButton(distanceBtnRef, sceneView);
+        break;
+      case "area":
+        activeWidget = new AreaMeasurement3D({
+          view: sceneView
+        });
+
+        // skip the initial 'new measurement' button
+        activeWidget.viewModel.start();
+
+        sceneView.ui.add(activeWidget, 'top-right');
+        initializationUtility.setActiveButton(areaBtnRef, sceneView);
+        break;
+      case null:
+      default:
+        if (activeWidget) {
+          sceneView.ui.remove(activeWidget);
+          activeWidget.destroy();
+          activeWidget = null;
+        }
+        break;
     }
-  
-    //sets the active button for styling
-    function setActiveButton(selectedButton) {
-      // focus the view to activate keyboard shortcuts for sketching
-      sceneView.focus();
-      const elements = document.getElementsByClassName("active");
-      for (let i = 0; i < elements.length; i++) {
-        elements[i].classList.remove("active");
-      }
-      if (selectedButton) {
-        selectedButton.classList.add("active");
-      }
+  },
+
+  //sets the active button for styling
+  setActiveButton: (selectedButton, sceneView) => {
+    // focus the view to activate keyboard shortcuts for sketching
+    sceneView.focus();
+    const elements = document.getElementsByClassName("active");
+    for (let i = 0; i < elements.length; i++) {
+      elements[i].classList.remove("active");
+    }
+    if (selectedButton) {
+      selectedButton.classList.add("active");
     }
   }
 };
